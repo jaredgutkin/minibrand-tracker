1d1dc8e5e28c95ad92cd5eaac508cb5b5ce9f05b
Jared Gutkin <jgutkinsam@gmail.com>
2022-08-07 12:20:14 -0500

    routing starting to get set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/NavbarComponent';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/Home';
+import Mbs4List from './components/mbs4/mbs4ListComponent';
 
 function App() {
   return (
     <Router>
       <NavbarComponent />
-      <Home />
+      <br />
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route exact path='/mbs4' element={<Mbs4List />}></Route>
+
+
+      </Routes>
+      <br />
     </Router>
     
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,46 +5,72 @@ import Mbs4List from "./mbs4/mbs4ListComponent";
 
 export default function Home() {
     return (
-      < Mbs4List />
-        // <div className="container">
-        //   <div className="row">
-        //     <div className="col-sm-6">
-        //       <Link to="/bug">
-        //         <div className="jumbotron jumbotron-fluid bg-success text-white">
-        //           <div className="container">
-        //             <h1 className="display-4 text-center">
-        //               <FaBug /> Bug
-        //             </h1>
-        //           </div>
-        //         </div>
-        //       </Link>
-        //     </div>
-        //     <div className="col-sm-6">
-        //       <Link to="/fish">
-        //         <div className="jumbotron jumbotron-fluid bg-primary text-white">
-        //           <div className="container">
-        //             <h1 className="display-4 text-center">
-        //               <FaFish /> Fish
-        //             </h1>
-        //           </div>
-        //         </div>
-        //       </Link>
-        //     </div>
-        //   </div>
-        //   <div className="row">
-        //     <div className="col-sm-12">
-        //       <Link to="/about">
-        //         <div className="jumbotron jumbotron-fluid bg-secondary
-        //           text-white">
-        //           <div className="container">
-        //             <h1 className="display-4 text-center">
-        //               <FaQuestionCircle /> About
-        //             </h1>
-        //           </div>
-        //         </div>
-        //       </Link>
-        //     </div>
-        //   </div>
-        // </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <Link to="/bug">
+                <div className="jumbotron jumbotron-fluid bg-success text-white">
+                  <div className="container">
+                    <h1 className="display-4 text-center">
+                      <FaBug /> Mini Brands! Series 1
+                    </h1>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <div className="col-sm-6">
+              <Link to="/fish">
+                <div className="jumbotron jumbotron-fluid bg-primary text-white">
+                  <div className="container">
+                    <h1 className="display-4 text-center">
+                      <FaFish /> Mini Brands! Series 2
+                    </h1>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-sm-6">
+              <Link to="/bug">
+                <div className="jumbotron jumbotron-fluid bg-success text-white">
+                  <div className="container">
+                    <h1 className="display-4 text-center">
+                      <FaBug /> Mini Brands! Series 3
+                    </h1>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <div className="col-sm-6">
+              <Link to="/mbs4">
+                <div className="jumbotron jumbotron-fluid bg-primary text-white">
+                  <div className="container">
+                    <h1 className="display-4 text-center">
+                      <FaFish /> Mini Brands! Series 4
+                    </h1>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+
+          
+          <div className="row">
+            <div className="col-sm-12">
+              <Link to="/about">
+                <div className="jumbotron jumbotron-fluid bg-secondary
+                  text-white">
+                  <div className="container">
+                    <h1 className="display-4 text-center">
+                      <FaQuestionCircle /> About
+                    </h1>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
       )
 }
